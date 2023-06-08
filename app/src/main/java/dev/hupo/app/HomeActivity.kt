6189bbf23408aaf8b8dc186364c41d855920cce4
hupo <liangrui-tianshi@qq.com>
2023-06-08 18:05:24 +0800

    demo

---
+++ app/src/main/java/dev/hupo/app/HomeActivity.kt
@@ -0,0 +1,13 @@
+package dev.hupo.app
+
+import android.app.Activity
+import android.os.Bundle
+
+class HomeActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
+
